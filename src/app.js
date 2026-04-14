ed36ef7c2b26594b57f99ba73870eea32a031151
mitulgadhiya3706 <mitulgadhiya23@gmail.com>
2026-04-14 15:01:18 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the server.")
+app.get("/user", (req, res) => {
+    res.send({firstname: "Mitul", lastname: "Gadhiya"})
+});
+
+app.post("/user", (req, res) => {
+    //saving data to DB
+    res.send("Data successfully saved to DB.")
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted from DB.")
 });
 
 app.use("/ipl", (req, res) => {
     res.send("ipl scoreboard")
 });
 
-app.use("/", (req, res) => {
-    res.send("Go to the dashboard.")
-});
+// app.use("/test", (req, res) => {
+//     res.send("Hello from the server.")
+// });
+
+
+// app.use("/", (req, res) => {
+//     res.send("Go to the dashboard.")
+// });
 
 
 app.listen(7777, () => {
